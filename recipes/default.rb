031ff0145f4eb05bb63404b07431c360048080c8
lector57 <lector@tut.by>
2018-11-28 00:28:03 +0300

    task9

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+yum_package 'yum-utils'
+
+execute 'yum-config-manager' do
+  command 'yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo'
+end
+
+yum_package 'docker-ce'
+
+execute 'docker registries-insecure-config' do
+  command 'echo { "insecure-registries" : ["ip_address:5000" ] } > /etc/docker/daemon.json'
+end
+
+service "docker" do
+  action :start
+end
