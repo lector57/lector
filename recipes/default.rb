203c34ab316fa71f2a52c0f7a538bddf2f9a7444
lector57 <lector@tut.by>
2018-12-12 06:35:21 +0300

    task10

---
+++ recipes/default.rb
@@ -0,0 +1,45 @@
+# Cookbook:: task10
+# Recipe:: default
+
+template '/tmp/docker_up.sh' do
+  source 'docker.erb'
+  mode '0744'
+  owner 'root'
+  group 'root'
+end
+
+template '/tmp/metadata.pl' do
+  source 'metadata.erb'
+  mode '0744'
+  owner 'root'
+  group 'root'
+end
+
+template '/tmp/environment.pl' do
+  source 'environment.erb'
+  mode '0744'
+  owner 'root'
+  group 'root'
+end
+
+puts #{node["docker"]["version"]}
+
+bash '/tmp/metadata' do
+  user 'root'
+  cwd '/tmp'
+  code '/tmp/metadata.pl'
+end
+
+
+bash 'docker_up' do
+  user 'root'
+  cwd '/tmp'
+  code '/tmp/docker_up.sh'
+end
+
+
+bash 'environment' do
+  user 'root'
+  cwd '/tmp'
+  code '/tmp/environment.sh'
+end
